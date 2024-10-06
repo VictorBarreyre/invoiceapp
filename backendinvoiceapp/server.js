3d57e5d20f47e21dc018ffaa7fcd5f8f28b2e757
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-06 17:01:56 +0200

    cors

--- backendinvoiceapp/server.js
+++ backendinvoiceapp/server.js
@@ -38,8 +38,8 @@ app.use((req, res, next) => {
 // Limiter à 50mb pour l'analyse des données JSON
 app.use(express.json({ limit: '50mb' }));
 
-// Configurer CORS
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173'];
+const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://www.dbill.io'];
+
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
@@ -48,7 +48,9 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Autoriser ces méthodes HTTP
+  allowedHeaders: ['Content-Type', 'Authorization'], // Autoriser ces en-têtes
+  credentials: true, // Permettre l'envoi de cookies et autres en-têtes sensibles
 }));
 
 // Configurer les fichiers statiques
