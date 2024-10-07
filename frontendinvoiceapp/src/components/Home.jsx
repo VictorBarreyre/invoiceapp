310ef7ad796471705d343b72fd3fa5e30ce14e04
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-07 11:38:12 +0200

    centré

--- frontendinvoiceapp/src/components/Home.jsx
+++ frontendinvoiceapp/src/components/Home.jsx
@@ -8,12 +8,38 @@ const Home = () => {
 
     return (
         <>
-            <Flex direction='column' justifyContent='center' >
-                <Heading fontSize={{ base: '20px', lg: '26px' }}>Powerful for developers. Fast for everyone.</Heading>
-                <Text> Lorem ipsum</Text>
-                <Button w={{ base: 'unset', lg: 'fit-content' }} color='white' borderRadius='30px' backgroundColor='black' p='10px 20px 10px 20px'> Créer ma facture</Button>
-                <Button w={{ base: 'unset', lg: 'fit-content' }} color='white' borderRadius='30px' backgroundColor='black' p='10px 20px 10px 20px'> À propos</Button>
+            <Flex
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                height="100vh"  // Pour centrer verticalement
+            >
+                <Heading fontSize={{ base: '20px', lg: '26px' }} textAlign="center">
+                    Powerful for developers. Fast for everyone.
+                </Heading>
+                <Text textAlign="center">Lorem ipsum</Text>
+                <Button
+                    w={{ base: 'unset', lg: 'fit-content' }}
+                    color="white"
+                    borderRadius="30px"
+                    backgroundColor="black"
+                    p="10px 20px"
+                    mt="20px" // Espacement entre les boutons et le texte
+                >
+                    Créer ma facture
+                </Button>
+                <Button
+                    w={{ base: 'unset', lg: 'fit-content' }}
+                    color="white"
+                    borderRadius="30px"
+                    backgroundColor="black"
+                    p="10px 20px"
+                    mt="10px" // Espacement entre les deux boutons
+                >
+                    À propos
+                </Button>
             </Flex>
+
         </>
 
     );
