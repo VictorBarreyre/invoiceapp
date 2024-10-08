9e748a2b838093b26aa72e95c8f72926f451d453
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-10-08 11:58:33 +0200

    mob-carrousel

--- frontendinvoiceapp/src/components/Carousel.jsx
+++ frontendinvoiceapp/src/components/Carousel.jsx
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Flex, Box, Image, IconButton, HStack } from '@chakra-ui/react';
+import { Flex, Box, Image, IconButton, HStack, useBreakpointValue } from '@chakra-ui/react';
 import { AiFillCaretRight, AiFillCaretLeft } from 'react-icons/ai';
 import Fact from '../assets/fact.png';
 import Rel from '../assets/rel.png';
 import Send from '../assets/send.png';
+import MobFact from '../assets/mob-fact.png';
+import MobRel from '../assets/mob-rel.png';
+import MobSend from '../assets/mob-send.png';
 
-const images = [Fact, Rel, Send];
+const desktopImages = [Fact, Rel, Send];
+const mobileImages = [MobFact, MobRel, MobSend];
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+
+    // Utilisation des images en fonction de la taille de l'écran
+    const images = useBreakpointValue({ base: mobileImages, md: desktopImages });
 
     // Fonction pour passer à l'image suivante automatiquement
     useEffect(() => {
@@ -16,7 +23,7 @@ const Carousel = () => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
         }, 3000); // Change l'image toutes les 3 secondes
         return () => clearInterval(interval);
-    }, []);
+    }, [images]);
 
     // Fonction pour aller à une image précise
     const goToImage = (index) => {
@@ -24,7 +31,7 @@ const Carousel = () => {
     };
 
     return (
-        <Flex direction="column" alignItems="center" w="75rem" position="relative">
+        <Flex direction="column" alignItems="center" w={{ base: '100%', md: '75rem' }} position="relative">
             {/* Les puces indicatrices */}
             <HStack spacing={2} mb={4}>
                 {images.map((_, index) => (
