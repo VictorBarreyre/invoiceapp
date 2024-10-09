ea2ecdc6ec394a4b74ea0c537530ca407d194f31
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-10-09 17:11:48 +0200

    tesstt

--- backendinvoiceapp/server.js
+++ backendinvoiceapp/server.js
@@ -17,21 +17,16 @@ const app = express();
 // Configuration de CORS
 const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://www.dbill.io'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Autoriser ces méthodes HTTP
-  allowedHeaders: ['Content-Type', 'Authorization'], // Autoriser ces en-têtes
-  credentials: true, // Permettre l'envoi de cookies et autres en-têtes sensibles
-}));
-
-// Répondre aux requêtes préflight OPTIONS
-app.options('*', cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins.includes(req.headers.origin) ? req.headers.origin : "");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Logger les requêtes reçues pour déboguer
 app.use((req, res, next) => {
