d5b14593c750511171898e7bb50c5306716a7f37
Victor Barreyre <barreyrevictor.contact@gmail.com>
2024-10-10 11:28:13 +0200

    remove console.log

--- frontendinvoiceapp/vite.config.js
+++ frontendinvoiceapp/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteBabel from 'vite-plugin-babel';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteBabel() // Ajout du plugin Babel pour Vite
+  ],
+  esbuild: {
+    drop: process.env.NODE_ENV === 'production' ? ['console'] : [],
+  }
+});
