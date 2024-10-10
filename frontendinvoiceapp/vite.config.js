96c3ce1460d3d696035d17265f1f57808de92872
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-10 15:39:07 +0200

    common

--- frontendinvoiceapp/vite.config.js
+++ frontendinvoiceapp/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteBabel from 'vite-plugin-babel';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
   plugins: [
