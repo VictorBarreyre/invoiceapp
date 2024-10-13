2fed5a0946c618da94490b5bf58e4b5457e71783
Vr <vr@MacBook-Pro-de-Vr.local>
2024-10-13 14:21:53 +0200

    tr home

--- frontendinvoiceapp/src/components/Home.jsx
+++ frontendinvoiceapp/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import {
     Flex, Heading, Text, Button, VStack, HStack, Box, Icon, SimpleGrid, StackDivider, Image
